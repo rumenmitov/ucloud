e7e5210f6330a210e9431749dbc08cee679d80fb
rumen <rumen.valmitov@gmail.com>
2023-01-28 00:59:19 +0100

    Fixing small typo

--- public/login/login.js
+++ public/login/login.js
@@ -2,7 +2,7 @@ let xhttp = new XMLHttpRequest();
 xhttp.open('get', 'https://ucloudproject.com/checkLogin/');
 xhttp.setRequestHeader("Content-type", "application/json");
 xhttp.withCredentials = true;
-xhttp.send(null));
+xhttp.send(null);
 
 xhttp.onload = function() {
 	if (this.responseText) location.href = 'https://ucloudproject.com/homePage/homePage.html?pwd=' + this.responseText;
