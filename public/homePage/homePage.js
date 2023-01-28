ad874b08d19fb047a57058365f861c836f75bd67
rumen <rumen.valmitov@gmail.com>
2023-01-28 15:28:17 +0100

    Fixing profile menu

--- public/homePage/homePage.js
+++ public/homePage/homePage.js
@@ -36,6 +36,11 @@ function getAllFiles(dir) {
         let avatar = document.createElement('img');
 	avatar.id = 'avatar';
         avatar.src = responseOBJ.avatarPath;
+   avatar.addEventListener('click', function() {
+          if (localStorage.getItem('ucloud')) domOBJ.profile_menu.style = 'display:block;';
+          else domOBJ.login.style = 'display:block;';
+});
+
         section.appendChild(avatar);
 
         let greeter = document.createElement('p');
@@ -301,11 +306,6 @@ window.onload = () => {
         let storage = JSON.parse(localStorage.getItem('ucloud'));
         location.href = `https://ucloudproject.com/homePage/homePage.html?pwd=${storage.username}`;
     });
-
-domOBJ.avatar.addEventListener('click', function() {
-       if (localStorage.getItem('ucloud')) domOBJ.profile_menu.style = 'display:block;';
-          else domOBJ.login.style = 'display:block;';
-}); 
 
     domOBJ.avatar_li.addEventListener('click', function(e) {
         domOBJ.avatarForm.style = 'display:block';
