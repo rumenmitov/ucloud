80a664d03cbed00e69a89afa44c941f9a2322fac
rumen <rumen.valmitov@gmail.com>
2023-04-01 19:40:42 +0200

    fixing search users issue

--- app.js
+++ app.js
@@ -712,7 +712,8 @@ searchUsersRouter.route('/:search_query').get((req, res) => {
       results.forEach(user =>{
 	user = user.split('/');
 	// output: /home/rumencho/ucloud/public/<user + ...>   <--- array
-	if (!user[5]) {
+	console.log(user);
+	if (!user[4]) {
 	  user = user[user.length - 1];
 	  if (!user.split('.')[1] && !user.split('/')[4]) {
 	    responseArray.push({
